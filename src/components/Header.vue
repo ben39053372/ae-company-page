fe90a5fdf900002ffac0b4761f46f2af87383917
Ben Cheng <bencheng@skylight-group.com>
2021-07-23 16:28:20 +0800

    feat: Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <a>CompanyLogo</a>
+    </div>
+    <div class="links">
+      <a>Home</a>
+      <a>Contact</a>
+      <a>About</a>
+    </div>
+  </div>
+</template>
+<script>
+  import { defineComponent } from '@vue/runtime-core';
+
+  export default defineComponent({});
+</script>
+
+<style scoped>
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10px;
+    background-color: #eae0d5;
+  }
+
+  .links > * {
+    margin: 5px 10px;
+  }
+</style>
