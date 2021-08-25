619b842b293b9fa0759bb84ec97f1aba5c8632ca
BenCheng <ben39053372@gmail.com>
2021-08-25 11:02:09 +0800

    config: i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,15 +1,21 @@
 import { createI18n } from 'vue-i18n';
 import en from './en.json';
+import zh_HK from './zh-HK.json';
+import zh_CN from './zh-CN.json';
 
 const messages = {
   en,
   'en-US': en,
+  zh: zh_HK,
+  'zh-HK': zh_HK,
+  'zh-CN': zh_CN,
 };
 
 export const i18n = createI18n({
-  legacy: false,
-  locate: navigator.language,
+  // legacy: false,
+  locale: 'zh-HK',
   messages: messages,
-  silentFallbackWarn: true,
-  silentTranslationWarn: true,
+  fallbackLocale: 'zh-HK',
+  // silentFallbackWarn: true,
+  // silentTranslationWarn: true,
 });
