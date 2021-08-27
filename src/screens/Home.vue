459bdf75d1f593c4f43362a4cba62d4c1c61d834
BenCheng <ben39053372@gmail.com>
2021-08-27 18:00:42 +0800

    assets: images

--- src/screens/Home.vue
+++ src/screens/Home.vue
@@ -1,4 +1,4 @@
-<template>
+<template lang="">
   <div class="home">
     <HomeBanner />
     <CompanyProfile />
@@ -7,31 +7,12 @@
     <ContactUs />
   </div>
 </template>
-<script>
-  import { defineComponent } from 'vue';
-  import { useI18n } from 'vue-i18n';
+<script setup>
   import CompanyProfile from '../components/home/CompanyProfile.vue';
   import OfficeEnvironment from '../components/home/OfficeEnvironment.vue';
   import CompanyCulture from '../components/home/CompanyCulture.vue';
   import ContactUs from '../components/home/ContactUs.vue';
   import HomeBanner from '../components/home/Banner.vue';
-
-  export default defineComponent({
-    name: 'Home',
-    components: {
-      CompanyProfile,
-      OfficeEnvironment,
-      CompanyCulture,
-      ContactUs,
-      HomeBanner,
-    },
-    setup() {
-      const { t } = useI18n();
-      return {
-        t,
-      };
-    },
-  });
 </script>
 
 <style scoped>
