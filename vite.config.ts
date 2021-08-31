19cba206b354ddb6a64ca5f14bb3185ca00b0f68
BenCheng <ben39053372@gmail.com>
2021-08-31 09:44:08 +0800

    chore: add more browser support  * but still not suppport IE, need revamp to vue 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import viteESLint from '@ehutch79/vite-eslint';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteESLint()],
+  plugins: [
+    vue(),
+    viteESLint(),
+    legacy({
+      targets: ['ie >= 10'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
   server: {
     fs: {
       strict: false,
